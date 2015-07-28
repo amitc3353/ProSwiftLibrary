758bac25da7845981d5649c5629a1a5d2a348623
Amit Chandel <amit@prolificinteractive.com>
2015-07-27 20:12:14 -0400

    Initial Commit

---
+++ ProSwiftLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProSwiftLibrary
+//
+//  Created by Amit Chandel on 7/27/15.
+//  Copyright (c) 2015 Amit Chandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
